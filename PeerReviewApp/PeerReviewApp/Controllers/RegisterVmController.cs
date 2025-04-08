ea94133363d6bef5ebfebfa583dabcb9d3b46367
Jason Bruce <46138154+Jasonbruce2001@users.noreply.github.com>
2025-04-08 11:05:49 -0700

    Initial Commit 4/8/25
    Added registration and login page, basic controllers,

---
+++ PeerReviewApp/PeerReviewApp/Controllers/RegisterVmController.cs
@@ -0,0 +1,86 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using PeerReviewApp.Models;
+
+namespace PeerReviewApp.Controllers;
+
+public class RegisterVmController : Controller
+{
+    private UserManager<AppUser> _userManager; 
+    private SignInManager<AppUser> _signInManager;
+    public RegisterVmController(UserManager<AppUser> userMngr, SignInManager<AppUser> signInMngr)
+    {
+        _userManager = userMngr; _signInManager = signInMngr;
+    } 
+    
+    // GET
+    [HttpGet]
+    public IActionResult Register()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Register(RegisterVm model)
+    {
+        if (ModelState.IsValid)
+        {
+            var user = new AppUser() { UserName = model.Username, AccountAge = DateTime.Now};
+            var result = await _userManager.CreateAsync(user, model.Password);
+
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, isPersistent: false);
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+        }
+        
+        return View(model);
+    }
+    
+    [HttpGet]
+    public IActionResult LogIn(string returnURL = "")
+    {
+        var model = new LogInVM { ReturnUrl = returnURL };
+        return View(model);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> LogIn(LogInVM model)
+    {
+        if (ModelState.IsValid)
+        {
+            var result = await _signInManager.PasswordSignInAsync
+                (model.Username, model.Password, model.RememberMe, false);
+            if (result.Succeeded)
+            {
+                if (!string.IsNullOrEmpty(model.ReturnUrl) &&
+                    Url.IsLocalUrl(model.ReturnUrl))
+                {
+                    return Redirect(model.ReturnUrl);
+                }
+                else
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+        }
+
+        ModelState.AddModelError(string.Empty, "Invalid username or password.");
+        return View(model);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> LogOut()
+    {
+        await _signInManager.SignOutAsync();
+        return RedirectToAction("Index", "Home");
+    }
+}
