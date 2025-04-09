1a3339d394a17eea1e7caa9d531dc01b6c395438
Jason Bruce <46138154+Jasonbruce2001@users.noreply.github.com>
2025-04-08 17:31:36 -0700

    Added Class list

--- PeerReviewApp/PeerReviewApp/Models/DomainModels/AppUser.cs
+++ PeerReviewApp/PeerReviewApp/Models/DomainModels/AppUser.cs
@@ -9,5 +9,6 @@ namespace PeerReviewApp.Models
             public IList<string> RoleNames { get; set; } = null!;
             public string? InstructorCode { get; set; }
             public DateTime AccountAge { get; set; }
+            public IList<Class>? Classes { get; set; } = new List<Class>();
         }
 }
