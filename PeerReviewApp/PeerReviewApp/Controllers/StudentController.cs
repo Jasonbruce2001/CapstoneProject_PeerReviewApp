fe2972777c75e7ec0eeceff706d0ebf0cb806581
travisburns <travisjb0@gmail.com>
2025-05-01 19:40:39 -0700

    Student dashboard now Displays the courses active to student. 'My Current Courses'

--- PeerReviewApp/PeerReviewApp/Controllers/StudentController.cs
+++ PeerReviewApp/PeerReviewApp/Controllers/StudentController.cs
@@ -23,12 +23,67 @@ public class StudentController : Controller
     {
         var user = await _userManager.GetUserAsync(HttpContext.User);
         //temp change to repo calls
-        var classes = new List<Class>();
+        // var classes = new List<Class>();
+        var classes = await _classRepository.GetClassesForStudentAsync(user);
+
+        var enhancedClasses = new List<Class>();
+        foreach (var cls in classes)
+        {
+            // Get the complete class with ParentCourse loaded
+            var fullClass = await _classRepository.GetClassByIdAsync(cls.ClassId);
+            if (fullClass != null)
+            {
+                enhancedClasses.Add(fullClass);
+            }
+        }
+
         var reviewGroups = new List<ReviewGroup>(); 
         var documents = new List<Document>();
-        
+
+       
+        if (user != null)
+        {
+           
+            try
+            {
+                classes = (await _classRepository.GetClassesForStudentAsync(user)).ToList();
+            }
+            catch (Exception ex)
+            {
+                
+            }
+        }
+
         var model = new StudentDashVM(){ Classes = classes, User = user, ReviewGroups = reviewGroups, Documents = documents };
         
         return View(model);
+    }
+
+
+    public async Task<IActionResult> ViewCourses()
+    {
+        var user = await _userManager.GetUserAsync(HttpContext.User);
+        if (user == null)
+        {
+            return RedirectToAction("Login", "Account");
+        }
+
+        var classes = await _classRepository.GetClassesForStudentAsync(user);
+
+        var enhancedClasses = new List<Class>();
+        foreach (var cls in classes)
+        {
+            var fullClass = await _classRepository.GetClassByIdAsync(cls.ClassId);
+            if (fullClass != null)
+            {
+                enhancedClasses.Add(fullClass);
+            }
+        }
+
+        return View(enhancedClasses);
+
+
+
+        return View(classes);
     }
 }
