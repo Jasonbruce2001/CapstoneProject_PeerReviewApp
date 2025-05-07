e6ecf32665dd0764c96538a53acd51f0a1c0a986
travisburns <travisjb0@gmail.com>
2025-05-07 16:24:51 -0700

    added reset password feature. Not fully complete yet.

---
+++ PeerReviewApp/PeerReviewApp/Models/ResetStudentPasswordVM.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PeerReviewApp.Models
+{
+    public class ResetStudentPasswordVM
+    {
+        public string StudentId { get; set; }
+        public string StudentName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "New Password")]
+        public string Password { get; set; }
+
+        public string Token { get; set; }
+    }
+}
